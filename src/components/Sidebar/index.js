ae2b8a8edc791f963cfbc8d061f9d75a3dd2215a
Rahul96278 <117078155+Rahul96278@users.noreply.github.com>
2023-08-26 15:55:55 +0530

    Update index.js

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,5 @@
-import './index.scss';
-import {Link, NavLink} from 'react-router-dom'
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -7,75 +7,98 @@ import {
   faLinkedin,
   faGithub,
   faInstagram,
-
- 
-  
 } from '@fortawesome/free-brands-svg-icons'
 import {
   faHome,
   faUser,
   faEnvelope,
   faBars,
-  faClose
- 
+  faClose,
 } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react';
+import { useState } from 'react'
 
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false)
 
-const Sidebar =()=> {
-const[showNav, setShowNav] =useState(false);
-
-    return(
+  return (
     <div className="nav-bar">
-        <Link className='logo' to='/'>
-            <img src={LogoS} alt="logo" />
-           
-        </Link>
-        <nav className={showNav ? 'mobile-show' : ''}>
-        <NavLink exact="true" actvieclassname="active" to="/">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink
+          exact="true"
+          actvieclassname="active"
+          onClick={() => setShowNav(false)}
+          to="/"
+        >
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
         </NavLink>
-        <NavLink exact="true" actvieclassname="active" className="about-link" to="/about">
-        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        <NavLink
+          exact="true"
+          actvieclassname="active"
+          className="about-link"
+          onClick={() => setShowNav(false)}
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
         </NavLink>
-        <NavLink exact="true" actvieclassname="active" className="contact-link" to="/contact">
-        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        <NavLink
+          exact="true"
+          actvieclassname="active"
+          className="contact-link"
+          onClick={() => setShowNav(false)}
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
-        <FontAwesomeIcon 
-        onClick={()=>setShowNav(false)}
-        icon={faClose}
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className="close-icon"
+        />
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/rahul-vijayan-3281711bb"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Rahul96278"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://instagram.com/vj.rahul_27?igshid=ZGUzMzM3NWJiOQ=="
+          >
+            <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
         color="#ffd700"
         size="3x"
-        className="close-icon"
-        />
-        </nav>
-        <ul>
-         <li>
-            <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/rahul-vijayan-3281711bb'>
-                <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-            </a>
-         </li>
-         <li>
-            <a target="_blank" rel='noreferrer' href='https://github.com/Rahul96278'>
-                <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-            </a>
-         </li>
-         <li>
-            <a target="_blank" rel='noreferrer' href='https://instagram.com/vj.rahul_27?igshid=ZGUzMzM3NWJiOQ=='>
-                <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
-            </a>
-         </li>
-        </ul>
-
-        <FontAwesomeIcon 
-        onClick={()=> setShowNav(true)}
-        icon={faBars} 
-        color="#ffd700" 
-        size="3x" 
-        className="hamburger-icon"/>
-        
+        className="hamburger-icon"
+      />
     </div>
-    )
+  )
 }
 
-export default Sidebar;
+export default Sidebar
