158ba47f10da00f73a5d53411a6c6dd4187646ce
Rahul96278 <117078155+Rahul96278@users.noreply.github.com>
2023-08-26 15:29:23 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,16 @@ import {Link} from 'react-router-dom'
 import {useState} from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import Loader from 'react-loaders'
+import {
+    faCss3,
+    faGitAlt,
+    faHtml5,
+    faJava,
+    faJsSquare,
+    faReact,
+  } from '@fortawesome/free-brands-svg-icons';
+ 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 const Home =()=>{
@@ -42,6 +52,29 @@ const jobArray=['o','f','t','w','a','r','e',' ','e','n','g','i','n','e','e','r']
                 <h2>Frontend Developer /Backend java</h2>
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
             </div>
+
+            <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faJava} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
             
             <Loader type="pacman" />
         </div>
